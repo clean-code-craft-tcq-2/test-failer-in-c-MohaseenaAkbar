4717005fb54659756f2baf036a2058a3bec0a77b
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-02-02 06:41:38 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,33 +1,43 @@
 #include <stdio.h>
 #include <assert.h>
 
-int FormatColourMap(const char**,const char**);
-void PrintColourMap(int,int,const char** ,const char** );
+int Format_Colour(const char**,const char**);
+void Print_Colour(int,int,const char** ,const char** );
+void Print_Colour_Test(int,int,const char** ,const char** );
+
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int ColorMapArray_size = FormatColourMap(majorColor,minorColor);
+    int ColorMapArray_size = Format_Colour(majorColor,minorColor);
     return ColorMapArray_size;
  }
-int FormatColourMap(const char** majorColor,const char** minorColor)
+int Format_Colour(const char** majorColor,const char** minorColor)
 {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-        PrintColourMap(i,j,majorColor,minorColor);
+        Print_Colour(i,j,majorColor,minorColor);
         }
     }
     return i * j;
 }
 
-void PrintColourMap(int i,int j,const char** majorColor,const char** minorColor)
+int PairNumber;
+void Print_Colour(int i,int j,const char** majorColor,const char** minorColor)
+{
+    PairNumber= i * 5 + j;
+    printf("%d | %s | %s\n",PairNumber, majorColor[i], minorColor[i]);
+}
+ 
+void Print_Colour_Test(int i,int j,const char** majorColor,const char** minorColor)
 {
     printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
 }
-    
+
 int main() {
     int result = printColorMap();
     assert(result == 25);
+    assert(PairNumber==25);
     printf("All is well (maybe!)\n");
     return 0;
 }
