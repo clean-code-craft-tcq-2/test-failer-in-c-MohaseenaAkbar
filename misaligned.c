b9f419dac19343ff92e0d2f58dfb44be07a4343b
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-02-02 14:18:43 +0530

    Create misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,26 +1,45 @@
 #include <stdio.h>
 #include <assert.h>
 
-int Format_Colour(const char**,const char**,void (*P)(int,const char*,const char*));
+int Format_Colour(const char**,const char**);
 void Print_Colour(int,const char* ,const char* );
+int FormatColourStub(const char** majorColor,const char**,void (*P)(int,const char*,const char*))
 int PairNumber;
 
-int printColorMap(int (*Format_Colour_Test)(const char**,const char**,void (*p)(int,const char*,const char*)),void (*Print_Colour_Test)(int,const char*,const char*)) {
+int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int ColorMapArray_size = (*Format_Colour_Test)(majorColor,minorColor,Print_Colour_Test);
+    int ColorMapArray_size = Format_Colour(majorColor,minorColor);
     return ColorMapArray_size;
  }
-int Format_Colour(const char** majorColor,const char** minorColor,void (*Print_Colour_Test)(int,const char*,const char*))
+int Format_Colour(const char** majorColor,const char** minorColor)
 {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-        PairNumber= i * 5 + j + 1;
-        (*Print_Colour_Test)(PairNumber,majorColor[i],minorColor[j]);
+        PairNumber= i * 5 + j+1;
+        Print_Colour(PairNumber,majorColor[i],minorColor[j]);
         }
     }
     return i * j;
+}
+
+int FormatColourStub(const char** majorColor,const char** minorColor,void (*Print_Colour_Test)(int,const char*,const char*))
+{
+    int i = 0, j = 0,Test_Var=1;
+    for(i = 0; i < 5; i++) {
+        for(j = 0; j < 5; j++) {
+        PairNumber= i * 5 + j ;
+        if(PairNumber==Test_Var)
+        {
+            (*Print_Colour_Test)(PairNumber,majorColor[i],minorColor[j]);
+             Test_Var++;
+        }
+        else
+         return 0;
+        }
+    }
+    return Test_Var;
 }
 
 void Print_Colour(int PairNumber,const char* majorColor,const char* minorColor)
@@ -29,11 +48,14 @@ void Print_Colour(int PairNumber,const char* majorColor,const char* minorColor)
 } 
 
 int main() {
-    int (*Format_Colour_Test)(const char**,const char**,void (*p)(int,const char*,const char*))=Format_Colour;
+    int (*FormatColour)(const char**,const char**,void (*p)(int,const char*,const char*))=Format_Colour;
     void (*Print_Colour_Test)(int,const char*,const char*)=Print_Colour;
-    int result = printColorMap(Format_Colour_Test,Print_Colour_Test);
+    int result = printColorMap(FormatColour,Print_Colour_Test);
     assert(result == 25);
     assert(PairNumber==25);
+    int (*Format_ColourStub)(const char**,const char**,void (*p)(int,const char*,const char*))=FormatColourStub;
+    int result = printColorMap(Format_ColourStub,Print_Colour_Test);
+    assert(result == 26);
     printf("All is well (maybe!)\n");
     return 0;
 }
